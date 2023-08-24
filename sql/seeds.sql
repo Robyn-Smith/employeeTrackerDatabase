326b6d9ba1904bc3dfd2da9ee50f5be69362ba97
Robyn Smith <robynsmith21@hotmail.com>
2023-08-24 22:24:48 +0100

    added more seed data to test functionality

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,25 +1,25 @@
 INSERT INTO department (id, name)
-VALUES (1, "HR"),
-       (2, "Warehouse"),
-       (3, "Customer Service"),
-       (4, "Security");
+VALUES (1, "Education"),
+       (2, "Medicine"),
+       (3, "Culinary"),
+       (4, "Finance");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (1, "Forklift", 18000, 1),
-       (2, "Sales Assistant", 12000, 2),
-       (3, "Stock Audit", 13000, 2),
-       (4, "Front End", 11000, 3),
-       (5, "Checkout Operator", 11000, 3),
-       (6, "Self Scan Operator", 12500, 3),
-       (7, "Security", 20000, 4);
+VALUES (1, "Teacher", 30000, 1),
+       (2, "Teaching Assistant", 20000, 1),
+       (3, "Head Teacher", 60000, 1),
+       (4, "Doctor", 76000, 2),
+       (5, "Nurse", 33000, 2),
+       (6, "Chef", 24000, 3),
+       (7, "Accountant", 45000, 4);
        
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Robert", "Jefferson", 1, NULL),
-       (2, "Tony", "Blands", 1, NULL),
-       (3, "Craig", "Smith", 2, NULL),
-       (4, "Rose", "Goth", 2, NULL),
-       (5, "Tim", "Stake", 2, NULL),
-       (6, "Matt", "Brooke", 3, NULL),
-       (7, "Linda", "Bloom", 3, NULL),
-       (8, "Tracey", "Erricson", 4, NULL),
-       (9, "Sam", "Jefferson", 4, NULL);
+VALUES (1,'Robyn', 'Smith', 2, NULL),
+       (2,'Adam', 'Smart', 6, NULL),
+       (3,'Wren', 'Jones', 1, NULL),
+       (4,'Aisha', 'Sian', 3, 3),
+       (5,'Debbie', 'Mason', 5, NULL),
+       (6,'Annabelle', 'Harris', 7, NULL),
+       (7,'Kirsty', 'Walters', 1, 1),
+       (8,'Richard', 'James', 4, 5),
+       (9,'Dave', 'Lewis', 6, NULL);
