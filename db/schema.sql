aa9991483ee1de49bfb282be3dffa440271f481f
Robyn Smith <robynsmith21@hotmail.com>
2023-08-27 00:06:35 +0100

    removed unneeded comments

--- db/schema.sql
+++ db/schema.sql
@@ -16,16 +16,6 @@ CREATE TABLE role(
     FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
--- CREATE TABLE employee(
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     first_name VARCHAR(30),
---     last_name VARCHAR(30),
---     role_id INT,
---     manager_id INT,
---     FOREIGN KEY(role_id) REFERENCES role(id),
---     FOREIGN KEY(manager_id) REFERENCES employee(id)
--- );
-
 CREATE TABLE employee
 (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
